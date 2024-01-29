94c33d8b0100ebe72831af8409af54511fa3eb2a
Valentin Mamichel <valentin.mamichel.dijon@gmail.com>
2024-01-29 14:46:35 +0100

    bloc lien image widget

--- wp-content/themes/bricotips/functions.php
+++ wp-content/themes/bricotips/functions.php
@@ -6,10 +6,12 @@ function theme_enqueue_styles()
 {
     // Chargement du style.css du thème parent Twenty Twenty
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
-    // Chargement du css du thème enfant ==> fichier theme.css dans le dossier bricotips/css/theme.css
+    // Chargement du css/theme.css pour nos personnalisations
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
     // Chargement du /css/widgets/image-titre-widget.css pour notre widget image titre
     wp_enqueue_style('image-titre-widget', get_stylesheet_directory_uri() . '/css/widgets/image-titre-widget.css', array(), filemtime(get_stylesheet_directory() . '/css/widgets/image-titre-widget.css'));
+    // Chargement du /css/widgets/bloc-lien-image-widget.css pour notre widget bloc lien image
+    wp_enqueue_style('bloc-lien-image-widget', get_stylesheet_directory_uri() . '/css/widgets/bloc-lien-image-widget.css', array(), filemtime(get_stylesheet_directory() . '/css/widgets/bloc-lien-image-widget.css'));
     // Chargement du /css/shortcodes/banniere-titre.css pour notre shortcode banniere titre
     wp_enqueue_style('banniere-titre-shortcode', get_stylesheet_directory_uri() . '/css/shortcodes/banniere-titre.css', array(), filemtime(get_stylesheet_directory() . '/css/shortcodes/banniere-titre.css'));
 }
@@ -19,6 +21,7 @@ function theme_enqueue_styles()
 
 // On créer une class Widget Image_Titre_Widget dans un fichier à part pour pas surcharger le functions.php
 require_once(__DIR__ . '/widgets/ImageTitreWidget.php');
+require_once(__DIR__ . '/widgets/BlocLienImageWidget.php');
 
 
 
@@ -26,9 +29,13 @@ function register_widgets()
 {
     //On enregistre le widget avec la class Image_Titre_Widget
     register_widget('Image_Titre_Widget');
+    register_widget('Bloc_Lien_Image_Widget');
 }
 //On demander à wordpress de charger des widget selon la fonction register_widgets()
 add_action('widgets_init', 'register_widgets');
+
+
+
 
 
 /* SHORTCODES */
