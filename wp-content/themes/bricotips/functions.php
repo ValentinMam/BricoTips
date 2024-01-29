4950d64d21737a7702a4275293a68c6457be8561
Valentin Mamichel <valentin.mamichel.dijon@gmail.com>
2024-01-29 12:01:09 +0100

    THEME - css folder & theme.css file

--- wp-content/themes/bricotips/functions.php
+++ wp-content/themes/bricotips/functions.php
@@ -6,4 +6,6 @@ function theme_enqueue_styles()
 {
     // Chargement du style.css du thème parent Twenty Twenty
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+    // Chargement du css du thème enfant ==> fichier theme.css dans le dossier bricotips/css/theme.css
+    wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
 }
