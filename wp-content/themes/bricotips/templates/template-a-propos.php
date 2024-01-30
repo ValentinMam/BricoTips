9b2d922e35a202ad1b44109ad1f67c350f1177de
Valentin Mamichel <valentin.mamichel.dijon@gmail.com>
2024-01-30 06:46:47 +0100

    THEME - template ready

---
+++ wp-content/themes/bricotips/templates/template-a-propos.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Template Name: Template à propos
+
+ */
+
+get_header();
+?>
+
+<main id="site-content">
+
+    Nous sommes dans la page à propos
+
+</main><!-- #site-content -->
+
+<?php get_template_part('template-parts/footer-menus-widgets'); ?>
+
+<?php
+get_footer();
